a76fa271a149d1bd44e658581da15e75bcaa220e
setteducatis <setteducati.s@husky.neu.edu>
2019-02-14 00:13:30 -0500

    Created sudoku board class to take input from an input source and save the data as a numpy array. From there we can get the data as a numpy array or list of lists (thought this would be useful for recursive solution but so far numpy seems better)

--- sudoku_game/sudoku_board.py
+++ sudoku_game/sudoku_board.py
@@ -1,15 +1,22 @@
-import numpy
+import numpy as np
 import sudoku_input
 
 '''
-Way to solve
+This module stores the data for a given sudoku board found by the input module.
 '''
-class sudoku_board:
-    def __init__(self):
-        pass
+class SudokuBoard:
+    '''
+    Representation of the sudoku board data as a numpy square array
+    '''
 
-    def get_numpy(self):
-        pass
+    def __init__(self, input):
+        self.board = input.take_input()
 
-    def get_2d_array(self):
-        pass
+
+    def get_numpy(self, as_cell=True):
+        if as_cell:
+            return self.board.apply()
+        return self.board
+
+    def get_list_of_list(self):
+        return self.board.tolist()
